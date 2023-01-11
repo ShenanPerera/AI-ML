ce007e83823c72755ef83d116046562e770a9cc9
ShenanPerera <85660117+ShenanPerera@users.noreply.github.com>
2023-01-11 19:04:18 +0000

    input marks of 10 students - max - min - average

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-def factorial(num):
-  result = 1
+count = 1 
+max_mark = 0
+min_mark = 100
+average = 0
+total = 0
 
-  while (num >= 1):
-    result = result * num
-    num = num - 1
+while count <= 10:
+  marks  = int(input("Enter marks : "))
+  if max_mark < marks:
+    max_mark = marks
 
-  return result
+  if min_mark > marks :
+    min_mark = marks
+    
+  total = total + marks
+  count = count + 1
+  
+average = total / 10
 
-print(factorial(5))
-print(factorial(3))
+print ("maximum mark : " , max_mark)
+print ("minimum mark : " , min_mark)
+print ("average mark : " , average)
