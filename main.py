3a47d099fa8bcac686fefaf387c3956dac54ffab
ShenanPerera <85660117+ShenanPerera@users.noreply.github.com>
2023-01-11 16:46:42 +0000

    data type & variables

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Shenan Perera")
-print("Holy Cross College - Gampaha")
+no1 = 2
+no2 = 56
+print (no1)
+print(type(no2))
+
+no3 = 67.4
+print(type(no3))
+
+str1 = "Hello"
+str2 = 'scl'
+print(type(str2))
