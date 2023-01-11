f44ff54e5d32b960a8d2549b1cc6201bcb22b336
ShenanPerera <85660117+ShenanPerera@users.noreply.github.com>
2023-01-11 17:20:00 +0000

    Selection - input length and width to check whether it is a square or a rectangle

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-x = int(input("Enter length of Rectangle "))
-y = int(input("Enter width of Rectangle "))
+length = int(input("Enter length : "))
+width = int(input("Enter width : "))
 
-Area = x * y
-print("Area is " ,Area)
-
-perimeter = 2 * (x + y)
-print("Perimeter is ", perimeter)
+if(length == width):
+  print("Square")
+else:
+  print("Rectangle")
