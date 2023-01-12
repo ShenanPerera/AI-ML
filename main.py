681954488557d71dcc8895144e0c6ee51194ea3c
ShenanPerera <85660117+ShenanPerera@users.noreply.github.com>
2023-01-12 05:22:41 +0000

    dictonaries

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-x = ["apple" , "banana" , "berry" , 45, 56.7 , True]
+x = {"id" : 100 , "name" : "Saman" , "salary" : 50000.00}
 
-print(x[0:2])
-print(x[1:3])
-print(x[3:6])
-print()
-print(x[3:])
-print(x[:3])
+print(x)
+print(type(x))
 
-print()
-print(x[0:5])
+print(x["id"])
+print(x["name"])
+print(x["salary"])
+
+x["bonus"] = 1000.00
+print(x)
+
+x["salary"] = 60000.00
+print(x)
+
+x["address"] = {"no" : 51 , "street" : "1st Lane" , "town" : "Kandy"}
+
+x["telephone"] = {"0718567455" , "0778899345"}
+print(x)
+
+print(x["address"]["town"])
